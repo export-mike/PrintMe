618d03dd06e50a13411b8836507856e6c68fad96
Mike James <mike.james@trueclarity.co.uk>
2014-04-16 14:28:41 +0100

    Refactoring - more generic selector for print areas

--- printme.js
+++ printme.js
@@ -1,15 +1,20 @@
-(function ($){
-    $('a.print-me').on('click', function(){
-        var areaToPrintSelector = $(this).data('print-me-click');
-        $areaToPrint = $('#'+areaToPrintSelector);
-        $printMeAreas = $('[data-print-me]');
-        if($areaToPrint){
+(function ($) {
+    $('a.print-me').on('click', function () {
+        var areaToPrintSelector = $(this).data('print-me-click'),
+            $printMeAreas = $('[data-print-me]'),
+            $areaToPrint = '';
+
+        if (areaToPrintSelector.substr(0, 1) === '.' || areaToPrintSelector.substr(0, 1) === '#') {
+            $areaToPrint = $(areaToPrintSelector);
+        }
+
+        if ($areaToPrint.length) {
             $printMeAreas.hide();
             $areaToPrint.show();
             window.print();
             $printMeAreas.show();
-        }else{
-            console.error('Failed to find area to print. ID:#' + areaToPrintSelector);
+        } else {
+            console.error('Failed to find area to print: (use . or #)' + areaToPrintSelector);
         }
     });
-})($);
+})(jQuery);
